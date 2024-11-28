b20b6caed11ab9271871930c8d9916abcb6b7d59
Andrzej Szlachta <andrzej.sz123@gmail.com>
2024-11-28 23:52:05 +0100

    Add Calendar component and utils

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,46 +1,114 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
+import {
+  startOfWeek,
+  startOfMonth,
+  endOfWeek,
+  endOfMonth,
+  eachDayOfInterval,
+  isBefore,
+  endOfDay,
+  isToday,
+  subMonths,
+  addMonths,
+} from "date-fns";
+import { formatDate } from "../utils/formatDate";
+import { cc } from "../utils/cc";
+import { isSameMonth } from "date-fns/fp";
+import { capitalize } from "../utils/utils";
 
 export function Calendar() {
-  const [selectedMonth, setSelectedMonth] = useState(new Date())
-  const calendarDays = 
+  const [selectedMonth, setSelectedMonth] = useState(new Date());
+
+  const calendarDays = useMemo(() => {
+    const firstWeekStart = startOfWeek(startOfMonth(selectedMonth));
+    const lastWeekEnd = endOfWeek(endOfMonth(selectedMonth));
+    return eachDayOfInterval({
+      start: firstWeekStart,
+      end: lastWeekEnd,
+    });
+  }, [selectedMonth]);
+
   return (
     <div className="calendar">
       <div className="header">
-        <button className="btn">Today</button>
+        <button className="btn" onClick={() => setSelectedMonth(new Date())}>
+          Today
+        </button>
         <div>
-          <button className="month-change-btn">&lt;</button>
-          <button className="month-change-btn">&gt;</button>
+          <button
+            className="month-change-btn"
+            onClick={() => setSelectedMonth((m) => subMonths(m, 1))}
+          >
+            &lt;
+          </button>
+          <button
+            className="month-change-btn"
+            onClick={() => setSelectedMonth((m) => addMonths(m, 1))}
+          >
+            &gt;
+          </button>
         </div>
-        <span className="month-title">June 2023</span>
+        <span className="month-title">
+          {capitalize(
+            formatDate(selectedMonth, { month: "long", year: "numeric" })
+          )}
+        </span>
       </div>
       <div className="days">
-        <div className="day non-month-day old-month-day">
-          <div className="day-header">
-            <div className="week-name">Sun</div>
-            <div className="day-number">28</div>
-            <button className="add-event-btn">+</button>
-          </div>
-          <div className="events">
-            <button className="all-day-event blue event">
-              <div className="event-name">Short</div>
-            </button>
-            <button className="all-day-event green event">
-              <div className="event-name">
-                Long event name that just keeps going
-              </div>
-            </button>
-            <button className="event">
-              <div className="color-dot blue"></div>
-              <div className="event-time">7am</div>
-              <div className="event-name">Event Name</div>
-            </button>
-          </div>
-        </div>
+        {calendarDays.map((day, index) => (
+          <CalendarDay
+            key={day.getTime()}
+            day={day}
+            showWeekName={index < 7}
+            selectedMonth={selectedMonth}
+          />
+        ))}
       </div>
     </div>
   );
 }
 
-function CalendarDay() {
+type CalendarDayProps = {
+  day: Date;
+  showWeekName: boolean;
+  selectedMonth: Date;
+};
 
+function CalendarDay({ day, showWeekName, selectedMonth }: CalendarDayProps) {
+  return (
+    <div
+      className={cc(
+        "day",
+        !isSameMonth(day, selectedMonth) && "non-month-day",
+        isBefore(endOfDay(day), new Date() && "old-month-day")
+      )}
+    >
+      <div className="day-header">
+        {showWeekName && (
+          <div className="week-name">
+            {formatDate(day, { weekday: "short" })}
+          </div>
+        )}
+        <div className={cc("day-number", isToday(day) && "today")}>
+          {formatDate(day, { day: "numeric" })}
+        </div>
+        <button className="add-event-btn">+</button>
+      </div>
+      {/* <div className="events">
+        <button className="all-day-event blue event">
+          <div className="event-name">Short</div>
+        </button>
+        <button className="all-day-event green event">
+          <div className="event-name">
+            Long event name that just keeps going
+          </div>
+        </button>
+        <button className="event">
+          <div className="color-dot blue"></div>
+          <div className="event-time">7am</div>
+          <div className="event-name">Event Name</div>
+        </button>
+      </div> */}
+    </div>
+  );
 }
